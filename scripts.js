379a7b66fe61138992693b0772f194fc9b6d76f1
Travis Thomas <tthomas027@gmail.com>
2021-07-18 12:13:04 -0700

    add function to display library on page

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+const container = document.querySelector('#container');
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -9,4 +10,28 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
+}
+
+function displayLibrary() {
+  myLibrary.forEach(book => {
+    const div = document.createElement('div');
+    div.classList.add('book')
+    const title = document.createElement('h2');
+    title.textContent = book.title;
+    const author = document.createElement('h3');
+    author.textContent = 'By: ' + book.author;
+    const pages = document.createElement('p');
+    pages.textContent = book.pages + ' pages long.'
+    const read = document.createElement('p');
+    if (book.read) {
+      read.textContent = 'Book finished.'
+    } else {
+      read.textContent = 'Book not finished yet.'
+    }
+    div.appendChild(title);
+    div.appendChild(author);
+    div.appendChild(pages);
+    div.appendChild(read);
+    container.appendChild(div);
+  })
 }
