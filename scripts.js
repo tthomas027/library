26212afe890641af2e36bb106b8c0fb756feb366
Travis Thomas <tthomas027@gmail.com>
2021-10-04 19:38:24 -0700

    Refactor library to use class

--- scripts.js
+++ scripts.js
@@ -1,8 +1,7 @@
-let myLibrary = [];
 const container = document.querySelector('#container');
 
-class Book{
-  constructor (title, author, pages, read) {
+class Book {
+  constructor(title, author, pages, read) {
     this.title = form.title.value;
     this.author = form.author.value;
     this.pages = form.pages.value;
@@ -13,49 +12,56 @@ class Book{
   }
 }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
+class Library {
+  constructor() {
+    this.books = [];
+  }
+  addBook(book) {
+    this.books.push(book);
+  }
+
+  displayLibrary() {
+    const books = document.querySelectorAll('.book');
+    books.forEach(book => {
+      container.removeChild(book);
+    })
+    this.books.forEach(book => {
+      const div = document.createElement('div');
+      div.classList.add('book');
+      div.id = this.books.indexOf(book);
+      const title = document.createElement('h2');
+      title.textContent = book.title;
+      const author = document.createElement('h3');
+      author.textContent = 'By: ' + book.author;
+      const pages = document.createElement('p');
+      pages.textContent = book.pages + ' pages long.'
+      const read = document.createElement('button');
+      if (book.read) {
+        read.textContent = 'Read';
+      } else {
+        read.textContent = 'Unread';
+      }
+      read.addEventListener('click', () => {
+        book.changeRead();
+        this.displayLibrary();
+      })
+      const deleteBtn = document.createElement('button');
+      deleteBtn.textContent = 'Delete Book'
+      deleteBtn.addEventListener('click', () => {
+        this.books.splice(div.id, 1);
+        this.displayLibrary();
+      })
+      div.appendChild(title);
+      div.appendChild(author);
+      div.appendChild(pages);
+      div.appendChild(read);
+      div.appendChild(deleteBtn);
+      container.appendChild(div);
+    })
+  }
 }
 
-function displayLibrary() {
-  const books = document.querySelectorAll('.book');
-  books.forEach(book => {
-    container.removeChild(book);
-  })
-  myLibrary.forEach(book => {
-    const div = document.createElement('div');
-    div.classList.add('book');
-    div.id = myLibrary.indexOf(book);
-    const title = document.createElement('h2');
-    title.textContent = book.title;
-    const author = document.createElement('h3');
-    author.textContent = 'By: ' + book.author;
-    const pages = document.createElement('p');
-    pages.textContent = book.pages + ' pages long.'
-    const read = document.createElement('button');
-    if (book.read) {
-      read.textContent = 'Read';
-    } else {
-      read.textContent = 'Unread';
-    }
-    read.addEventListener('click', () => {
-      book.changeRead();
-      displayLibrary();
-    })
-    const deleteBtn = document.createElement('button');
-    deleteBtn.textContent = 'Delete Book'
-    deleteBtn.addEventListener('click', () => {
-      myLibrary.splice(div.id, 1);
-      displayLibrary();
-    })
-    div.appendChild(title);
-    div.appendChild(author);
-    div.appendChild(pages);
-    div.appendChild(read);
-    div.appendChild(deleteBtn);
-    container.appendChild(div);
-  })
-}
+let myLibrary = new Library();
 
 const newBook = document.querySelector('#new');
 const modal = document.querySelector("#formModal");
@@ -81,7 +87,7 @@ form.addEventListener('submit', (e) => {
   modal.style.display = 'none';
 
   let newBook = new Book(title, author, pages, read);
-  addBookToLibrary(newBook);
-  displayLibrary();
+  myLibrary.addBook(newBook);
+  myLibrary.displayLibrary();
   form.reset();
 })
